2757896700c662256865db1dc5680cbf0c192f59
adoumadje <adoumadjecaleb@gmail.com>
2024-11-21 06:50:06 +0100

    come-back

--- pws2024-vue/src/App.vue
+++ pws2024-vue/src/App.vue
@@ -1,39 +1,30 @@
 <script>
-  export default {
-    data() {
-      return {
-        inputData: {
-          firstName: '',
-          yearOfBirth: 2000
-        }
+import DataInput from './components/DataInput.vue';
+
+export default {
+  data() {
+    return {
+      inputData: {
+        firstName: '',
+        yearOfBirth: 2000
       }
-    },
-    methods: {
-      vuetifyClicked() {
-        // console.log('button vuetify clicked', this.inputData);
-        fetch('/api', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json'
-          },
-          body: JSON.stringify(this.inputData)
-        })
-      }
-    },
-    mounted() {
-      console.log('mounted fired');
     }
+  },
+  components: { DataInput },
+  methods: {
+    
   }
+}
 </script>
 
 <template>
   <div>
-    <v-text-field variant="outlined" v-model="inputData.firstName" label="first name"></v-text-field>
-    <v-text-field variant="outlined" v-model="inputData.yearOfBirth" label="Year of birth" type="number"></v-text-field>
-    <v-btn variant="elevated" color="primary" @click="vuetifyClicked">Vuetify</v-btn>
+    <DataInput class="horizontalElement"></DataInput>
   </div>
 </template>
 
 <style scoped>
-
+  .horizontalElement {
+    width: 400px;
+  }
 </style>
---
+++ pws2024-vue/src/components/DataInput.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    data() {
+        return {
+            inputData: {},
+        }
+    }
+}
+</script>
+
+<template>
+    <v-form>
+        <v-card>
+            <v-card-title>
+                Enter data
+                <v-spacer></v-spacer>
+            </v-card-title>
+            <v-card-subtitle>
+                {{ inputData._id || 'to create' }}
+            </v-card-subtitle>
+            <v-card-text>
+                <v-text-field v-model="inputData.firstName" label="First Name" variant="outlined"></v-text-field>
+                <v-text-field v-model="inputData.lastName" label="Last Name" variant="outlined"></v-text-field>
+                <v-text-field v-model="inputData.birthDate" label="Birth Date" variant="outlined" type="date"></v-text-field>
+            </v-card-text>
+        </v-card>
+    </v-form>
+</template>
+
+<style scoped>
+
+</style>
