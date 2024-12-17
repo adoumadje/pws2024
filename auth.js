8e2391d6a35c711aa405402af4a012f48a1cfc2a
adoumadje <adoumadjecaleb@gmail.com>
2024-12-17 17:40:07 +0100

    added auth

---
+++ auth.js
@@ -0,0 +1,102 @@
+const crypto = require('crypto')
+const mongoose = require('mongoose')
+const uuid = require('uuid')
+
+const User = new mongoose.model('User', new mongoose.Schema({
+    _id: { type: String, default: uuid.v4 },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    roles: { type: [Number] }
+}, { versionKey: false }))
+
+const makeHash = (password) => {
+    return crypto.createHash('sha256').update(password).digest('base64')
+}
+
+const getIntersection = (arr1, arr2) => {
+    const lookupSet = new Set(arr1)
+    return arr2.filter((el) => lookupSet.has(el))
+}
+
+// create user 'admin' if it does not exist
+User.findOne({username: 'admin'})
+.then((user) => {
+    if(!user) {
+        const admin = new User({username: 'admin', password: 'admin', roles: [0]})
+        admin.save()
+    }
+})
+.catch((err) => {})
+
+// create user 'user' if it does not exist
+User.findOne({username: 'user'})
+.then((res) => {
+    if(!res) {
+        const user = new User({username: 'user', password: 'user', roles: [1]})
+        user.save()
+    }
+})
+
+const auth = module.exports = {
+    makeHash,
+
+    checkCredentials: (username, password, nextTick) => {
+        User.findOne({ username, password: auth.makeHash(password) })
+        .then((user) => nextTick(null, user || false))
+        .catch(err => nextTick(null, false))
+    },
+
+    checkIfInRole: (roleNums) => (req, res, nextTick) => {
+        let intersection = getIntersection(roleNums || [], req.user ? (req.user.roles || []) : [])
+        if(!req.isAuthenticated()) {
+            res.status(401).json({ error: 'Unauthorized' })
+        } else if(intersection.length > 0) {
+            return nextTick()
+        } else {
+            res.status(403).json({ error: 'Permission denied'})
+        }
+    },
+
+    serialize: (user, nextTick) => nextTick(null, user.username),
+
+    deserialize: (username, nextTick) => {
+        User.findOne({username})
+        .then((user) => {
+            if(user) {
+                return nextTick(null, user)
+            } else {
+                return nextTick(new Error('No such user'), null)
+            }
+        })
+        .catch((err) => ({ error: err.message }))
+    },
+
+    whoami: (req, res) => {
+        req.session.roles = req.user ? req.user.roles : []
+        req.session.save()
+        let data = { sessionid: req.session.id }
+        if(req.user) {
+            data.username = req.user.username
+            data.roles = req.user.roles
+        }
+        res.json(data)
+    },
+
+    login: (req, res) => auth.whoami(req, res),
+
+    logout: (req, res) => req.logout(() => auth.whoami(req, res)),
+
+    errorHandler: (err, req, res, nextTick) => res.status(401).json({ error: err.message }),
+
+    getUsers: (nextTick) => {
+        User.find()
+        .then((users) => {
+            users.foreach((user) => {
+                delete user._id
+                user.password = user.password ? 1 : 0
+            })
+            nextTick(null, users)
+        })
+        .catch((err) => nextTick(err, null))
+    } 
+}
--- server.js
+++ server.js
@@ -62,9 +62,9 @@ mongoose.connect(config.dbUrl)
     process.exit(0)
 })
 
+// Person Requests
 const personEndpoint = '/api/person'
 
-// Database transactions
 app.get(personEndpoint, (req, res) => {
     let sort = {}
     if(req.query.sort) {
@@ -144,6 +144,108 @@ app.delete(personEndpoint, (req, res) => {
         return
     }
     Person.findOneAndDelete({_id})
+    .then((row) => {
+        res.json(row)
+    })
+    .catch((err) => {
+        res.status(400).json({ error: err.message })
+    })
+})
+
+// Project Requests
+const projectSchema = new mongoose.Schema({
+    _id: { type: String, default: uuid.v4 },
+    name: { type: String, required: true, validate: {
+        validator: (v) => {
+            return /^\p{L}/u.test(v)
+        },
+        message: (props) => `${props.value} does not start from a letter`
+    } },
+    startDate: { type: Date, required: true, transform: (v) => v.toISOString().substr(0, 10) },
+    endDate: { type: Date, required: true, transform: (v) => v.toISOString().substr(0, 10) }
+},
+{
+    versionKey: false,
+    additionalProperties: false
+})
+
+let Project = null
+
+
+const projectEndpoint = '/api/project'
+
+app.get(projectEndpoint, (req, res) => {
+    let sort = {}
+    if(req.query.sort) {
+        sort[req.query.sort] = +req.query.order || 1
+    }
+    const matching = {
+        $match: {name: { $regex: req.query.search || '', $options: 'i' }}
+    }
+
+    const aggregation = [matching]
+
+    if(req.query.sort) {
+        aggregation.push({ $sort: sort})
+    }
+    aggregation.push({ $skip: +req.query.skip || 0 })
+    let limit = +req.query.limit
+    if(!isNaN(limit) && limit > 0) {
+        aggregation.push({ $limit: limit})
+    }
+
+    Project.aggregate([{ $facet: {
+        total: [ matching, { $count: 'count'} ],
+        data: aggregation
+    }}])
+    .then(facet => {
+        [facet] = facet
+        facet.total = ( facet.total && facet.total[0] ? facet.total.count : 0) || 0
+        facet.data = facet.data.map(project => new Project(project))
+        res.json(facet)
+    })
+    .catch(err => res.status(400).json({error: err.message}))
+})
+
+app.post(projectEndpoint, (req, res) => {
+    let project = new Project(req.body)
+    let err = person.validateSync()
+    if(err) {
+        res.status(400).json({ error: err.message })
+        return
+    }
+    project.save()
+    .then((row) => {
+        res.json(row)
+    })
+    .catch((err) => {
+        res.status(400).json({ error: err.message })
+    })
+})
+
+app.put(projectEndpoint, (req, res) => {
+    let _id = req.body._id
+    if(!_id) {
+        res.status(400).json({ error: 'no _id!'})
+        return
+    }
+    delete req.body._id
+    Person.findOneAndUpdate({_id}, { $set: req.body }, { new: true, runValidators: true })
+    .then((row) => {
+        res.json(row)
+    })
+    .catch((err) => {
+        res.status(400).json({ error: err.message })
+    })
+})
+
+app.delete(projectEndpoint, (req, res) => {
+    let _id = req.query._id
+    if(!_id) {
+        res.status(400).json({ error: 'no _id!'})
+        return
+    }
+    Project.findOneAndDelete({_id})
     .then((row) => {
         res.json(row)
     })
